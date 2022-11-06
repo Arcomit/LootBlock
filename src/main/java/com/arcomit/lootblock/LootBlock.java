c0081548147b6fe04cff5cd3bb013927c03fa282
Moflop <8089341@qq.com>
2022-11-06 18:31:59 +0800

    创建文件夹.JPG

---
+++ src/main/java/com/arcomit/lootblock/LootBlock.java
@@ -0,0 +1,10 @@
+package com.arcomit.lootblock;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(LootBlock.MODID)
+public class LootBlock {
+    public static final String MODID = "lootblock";
+    public static final String VERSION = "1.0.0";
+
+}
